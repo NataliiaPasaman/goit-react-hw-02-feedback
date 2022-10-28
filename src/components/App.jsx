ee380be229e201cbe06c97781a071326460b63d8
NataliiaPasaman <natapasaman@gmail.com>
2022-10-28 23:38:42 +0300

    made App class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,36 @@
-import { FeedbackWidget } from "./FeedbackWidget/feedbackWidget.jsx";
+import { Component } from "react";
+import { FeedbackWidget } from "./FeedbackWidget/FeedbackWidget.jsx";
+import { Statistics } from "./Statistics/Statistics.jsx";
 
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <FeedbackWidget />
-    </div>
-  );
-};
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+
+        <FeedbackWidget />
+        <Statistics />
+      </div>
+    );
+  }
+}
 
 
