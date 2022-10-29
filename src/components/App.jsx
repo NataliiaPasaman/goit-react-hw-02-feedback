d435bbca1244db8ef9d721c10eab2174b6e6e225
NataliiaPasaman <natapasaman@gmail.com>
2022-10-29 17:09:22 +0300

    refactoring component Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ export class App extends Component {
     };
   }
 
-  addFeedback = (event) => {
+  addFeedback = event => {
     this.setState(prevState => {
       return { [event.target.name]: prevState[event.target.name] + 1 };
     });
@@ -21,16 +21,15 @@ export class App extends Component {
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
     const total = good + neutral + bad;
-      return total;
-  }
+    return total;
+  };
 
   countPositiveFeedbackPercentage = () => {
     const { good } = this.state;
     const total = this.countTotalFeedback();
-    
     const positivePercentage = Number(((good * 100) / total).toFixed());
     return positivePercentage;
-  }
+  };
 
   render() {
     return (
@@ -44,14 +43,13 @@ export class App extends Component {
           color: '#010101',
         }}
       >
-        <FeedbackWidget
-          feedback={this.state}
-          addFeedback={this.addFeedback}
-        />
-        <Statistics 
-        feedback={this.state} 
-        totalFeedback={this.countTotalFeedback}
-        goodPercentage={this.countPositiveFeedbackPercentage}
+        <FeedbackWidget feedback={this.state} addFeedback={this.addFeedback} />
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          totalFeedback={this.countTotalFeedback}
+          positivePercentage={this.countPositiveFeedbackPercentage}
         />
       </div>
     );
@@ -61,8 +59,6 @@ export class App extends Component {
 /**Виконай рефакторинг застосунку. Стан застосунку повинен залишатися у 
  * кореневому компоненті <App>.
 
-Винеси відображення статистики в окремий компонент <Statistics good={} neutral={} 
-bad={} total={} positivePercentage={}>.
 Винеси блок кнопок в компонент <FeedbackOptions options={} onLeaveFeedback={}>.
 Створи компонент <Section title="">, який рендерить секцію із заголовком і дітей (children). 
 Обгорни кожен із <Statistics> і <FeedbackOptions> у створений компонент секції. */
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Statistics = ({ feedback, totalFeedback, goodPercentage }) => {
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  totalFeedback,
+  positivePercentage,
+}) => {
   const totalFeedbacks = totalFeedback();
-  const percentageOfGood = goodPercentage();
-  console.log('percentageOfGood', percentageOfGood);
+  const goodPercentage = positivePercentage();
   return (
     <>
       <h2>Statistics</h2>
       <ul>
-        <li>Good: {feedback.good}</li>
-        <li>Neutral: {feedback.neutral}</li>
-        <li>Bad: {feedback.bad}</li>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
         <li>Total: {totalFeedbacks}</li>
-        <li>Positive feedback: {percentageOfGood || 0}%</li>
+        <li>Positive feedback: {goodPercentage || 0}%</li>
       </ul>
     </>
   );
-}
-
+};
 
 Statistics.propypes = {
-  feedback: PropTypes.shape({
-      good: PropTypes.number.isRequired,
-      neutral: PropTypes.number.isRequired,
-      bad: PropTypes.number.isRequired,
-  }),
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
   totalFeedback: PropTypes.func.isRequired,
   goodPercentage: PropTypes.func.isRequired,
-}
+};
