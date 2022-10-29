6d4943678eaac5092613709541919439b6fa495e
NataliiaPasaman <natapasaman@gmail.com>
2022-10-29 18:46:27 +0300

    fixed Statistics feedbacks

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Notification } from 'components/Notification/Notification';
+
 
 export const Statistics = ({
   good,
@@ -10,14 +12,17 @@ export const Statistics = ({
 }) => {
   const totalFeedbacks = totalFeedback();
   const goodPercentage = positivePercentage();
-  return (
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {totalFeedbacks}</li>
-        <li>Positive feedback: {goodPercentage || 0}%</li>
-      </ul>
+
+  return (totalFeedbacks !== 0) ? (
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {totalFeedbacks}</li>
+      <li>Positive feedback: {goodPercentage || 0}%</li>
+    </ul>
+  ) : (
+    <Notification message="There is no feedback" />
   );
 };
 
